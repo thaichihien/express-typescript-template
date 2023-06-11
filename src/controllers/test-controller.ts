76a24644f325c57ba2f94ca4740a8dd5e4f48968
thaichihien <79949235+thaichihien@users.noreply.github.com>
2023-06-11 09:20:18 +0700

    add public folder for html,css

--- src/controllers/test-controller.ts
+++ src/controllers/test-controller.ts
@@ -1,16 +1,16 @@
 import {Request,Response} from 'express'
-
+import path from 'path'
 
 class TestController {
 
     testMain = async (req : Request,res : Response) =>  {
        
-        const serverinfo = {
-            "server" : "Express TypeScript Template",
-            "author" : "Chi Hien",
-            "release date" : "06/11/2023"
-        }
-        res.send(serverinfo)
+        // const serverinfo = {
+        //     "server" : "Express TypeScript Template",
+        //     "author" : "Chi Hien",
+        //     "release date" : "06/11/2023"
+        // }
+        res.sendFile(path.join(__dirname,'/public/index.html'))
     }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import appRouter from './routers/app-router'
 
 dotenv.config()
@@ -7,6 +8,10 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
+// - Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/',appRouter)
 
